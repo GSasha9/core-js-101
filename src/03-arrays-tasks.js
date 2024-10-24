c62863c13bb03763ea12ff2c0597aef9a2c8ae62
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2024-10-24 13:28:59 +0300

    feat:almosr done arrays. start conditions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -432,8 +432,15 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  arr.sort((a, b) => {
+    const countryComparison = a.country.localeCompare(b.country);
+    if (countryComparison !== 0) {
+      return countryComparison;
+    }
+    return a.city.localeCompare(b.city);
+  });
+  return arr;
 }
 
 /**
@@ -454,8 +461,14 @@ function sortCitiesArray(/* arr */) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  const arr = Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
+  arr.map((el, ind) => {
+    const p = el;
+    p[ind] = 1;
+    return p[ind];
+  });
+  return arr;
 }
 
 /**
@@ -471,8 +484,13 @@ function getIdentityMatrix(/* n */) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let el = start - 1;
+  const arr = Array.from({ length: end - start + 1 }, () => {
+    el += 1;
+    return el;
+  });
+  return arr;
 }
 
 /**
@@ -486,8 +504,15 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const res = [];
+  arr.map((el) => {
+    if (res.indexOf(el) === -1) {
+      res.push(el);
+    }
+    return res;
+  });
+  return res;
 }
 
 /**
--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -132,8 +132,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  return !(
+    rect1.left > rect2.left + rect2.width || rect1.left + rect1.width < rect2.left
+    || rect1.top > rect2.top + rect2.height
+    || rect1.top + rect1.height < rect2.top
+  );
 }
 
 
@@ -163,8 +167,11 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const dx = point.x - circle.center.x;
+  const dy = point.y - circle.center.y;
+  const distanceSq = dx * dx + dy * dy;
+  return distanceSq < circle.radius * circle.radius;
 }
 
 
@@ -179,8 +186,18 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  const arr = str.split('');
+  let result = null;
+  arr.some((el) => {
+    const fI = str.indexOf(el);
+    const lI = str.lastIndexOf(el);
+    if (fI === lI) {
+      result = el;
+    }
+    return result;
+  });
+  return result;
 }
 
 
